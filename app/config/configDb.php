8b3e6b2d018ae9bff8685c30eeff6089b7e064ca
Valerii <nyshnui@gmail.com>
2022-09-15 18:58:34 +0300

    qwed

--- app/config/configDb.php
+++ app/config/configDb.php
@@ -1,7 +1,7 @@
 <?php
 
 return $arr = [
-    'dsn' => "pgsql:host=ec2-54-85-56-210.compute-1.amazonaws.com;dbname=d58m022a2hvl3a;charset=utf8",
+    'dsn' => "pgsql:host=ec2-54-85-56-210.compute-1.amazonaws.com;dbname=d58m022a2hvl3a",
     'user' => "smhmyxvlicsxcv",
     'pass' => "2f1b11e7f5e27cd514c4f03326b926a75111fb1c4fc40e49679390495e0e67d5"
 ];
